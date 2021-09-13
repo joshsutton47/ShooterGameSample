f66765a7b3306cbfd070b16997bab6b0465a18b6
joshsutton47 <56270887+joshsutton47@users.noreply.github.com>
2021-09-13 16:41:41 -0500

    Player Object Created
    Created the player object
    Started the PlayerController script

---
+++ Shooter Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+/***
+ * Created by: Josh Sutton
+ * Date Created: September 13, 2021
+ * 
+ * Last Edited by:
+ * Last Updated: September 13, 2021
+ * 
+ * Description: Player control movements
+ ***/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
